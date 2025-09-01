86c3294e81e68d84bc95294d5b6a7073b13a58c8
brrodrigues88 <brrodrigues6003@gmail.com>
2025-09-01 20:50:36 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,72 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main() {
-    // Carta 1
-    char Codigo1[15] = "A01";
-    char Estado1[15] = "Paraná";
+    int Carta1, Carta2;
+    int opção;
+ 
+    //Carta 1
+    char Código1[15] = "A01";
+    char Estado1[15]= "Paraná";
     char Cidade1[15] = "Jacarezinho";
-    int Populacao1 = 40000;
-    float Area1 = 602;
+    int População1 = 40375;
+    float Área1 = 602;
     float PIB1 = 1598109262;
     int Turismo1 = 8;
-    float Densidade1 = Populacao1 / Area1;
-    float PIBperCapita1 = PIB1 / Populacao1;
 
-    // Carta 2
-    char Codigo2[15] = "A02";
-    char Estado2[15] = "São Paulo";
+    //Carta 2
+    char Código2[15] = "A02";
+    char Estado2[15]= "São Paulo";
     char Cidade2[15] = "Campinas";
-    int Populacao2 = 1214000;
-    float Area2 = 795;
-    float PIB2 = 72900000000; 
+    int População2 = 1214000;
+    float Área2 = 795;
+    float PIB2 = 72900000000;
     int Turismo2 = 15;
-    float Densidade2 = Populacao2 / Area2;
-    float PIBperCapita2 = PIB2 / Populacao2;
 
-    // Exibir Carta 1
-    printf("=== Carta 1 ===\n");
-    printf("Código: %s\n", Codigo1);
+    //Exibir Carta 1
+    printf("Carta1\n");
+    printf("Código: %s\n", Código1);
     printf("Estado: %s\n", Estado1);
     printf("Cidade: %s\n", Cidade1);
-    printf("Área: %.2f km²\n", Area1);
-    printf("PIB: %.2f Bilhoes de Reais\n", PIB1);
-    printf("Número de Pontos Turísticos: %d\n", Turismo1);
-    printf("Densidade Populacional: %.2f hab/km²\n", Densidade1);
-    printf("PIB per Capita: %.2f Mil Reais\n", PIBperCapita1);
+    printf("População: %d Mil habitantes\n", População1);
+    printf("Área: %.fkm²\n", Área1);
+    printf("PIB: %.f Bilhões de Reais\n", PIB1);
+    printf("Números de Pontos Turísticos: %d\n", Turismo1);
     printf("\n");
     
-    // Exibir Carta 2
-    printf("=== Carta 2 ===\n");
-    printf("Código: %s\n", Codigo2);
+    //Exibir Carta 2 
+    printf("Carta2\n");
+    printf("Código: %s\n", Código2);
     printf("Estado: %s\n", Estado2);
     printf("Cidade: %s\n", Cidade2);
-    printf("Área: %.2f km²\n", Area2);
-    printf("PIB: %.2f Bilhoes de Reais\n", PIB2);
-    printf("Número de Pontos Turísticos: %d\n", Turismo2);
-    printf("Densidade Populacional: %.2f hab/km²\n", Densidade2);
-    printf("PIB per Capita: %.2f Mil Reais\n", PIBperCapita2);
-    printf("\n");
+    printf("População: %d Milhão de habitantes\n", População2);
+    printf("Área: %.fkm²\n", Área2);
+    printf("PIB: %.f Bilhões de Reais\n", PIB2);
+    printf("Números de Pontos Turísticos: %d\n", Turismo2);
+    printf("\n"); 
 
-    // Comparacões
-    printf("=== COMPARAÇÃO ENTRE AS CARTAS ===\n");
-    
-    // Densidade Populacional
-    if (Densidade1 > Densidade2) {
-        printf("Densidade Populacional: %s Venceu!\n", Cidade1);
-    } else if (Densidade2 > Densidade1) {
-        printf("Densidade Populacional: %s Venceu!\n", Cidade2);
-    } else {
-        printf("Densidade Populacional: Empate!\n");
-    }
+    //Menus Interativo
+    printf("Escolha uma das opções abaixo para comparar as cartas:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("Digite a opção desejada: ");
+    scanf("%d", &opção);
 
-    // PIB per Capita
-    if (PIBperCapita1 > PIBperCapita2) {
-        printf("PIB per Capita: %s venceu!\n", Cidade1);
-    } else if (PIBperCapita2 > PIBperCapita1) {
-        printf("PIB per Capita: %s venceu!\n", Cidade2);
-    } else {
-        printf("PIB per Capita: Empate!\n");
-    }
+    //Comparar Cartas População e Área 
+if (População1 > População2) {
+    printf("# Jacarezinho-Pr Venceu! #\n"); 
+} else if (População1 < População2) {
+    printf("# Campinas-Sp Venceu! #\n");
+} else {
+    printf("# Houve um Empate! #\n");
 
+    if (Área1 > Área2) {
+    printf("# Jacarezinho-Pr Venceu! #\n"); 
+} else if (Área1 < Área2) {
+    printf("# Campinas-Sp Venceu! #\n");
+} else {
+    printf("# Houve um Empate! #\n");
+}
+}
     return 0;
 }
